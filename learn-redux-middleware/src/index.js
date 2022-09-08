88fea9a88e161f8781c1b10d1196cfc66116cd4e
yuj1818 <95585314+yuj1818@users.noreply.github.com>
2022-09-08 14:10:19 +0900

    220908

---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {createStore, applyMiddleware, compose} from 'redux';
+import rootReducer, {rootSaga} from "./modules";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger/src";
+import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger, thunk, sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
